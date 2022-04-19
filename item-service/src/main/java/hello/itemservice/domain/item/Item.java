b05a30d2a671a7bf29f649a3e63c5c82ad92a24e
soup930 <soup930>
2022-04-19 18:00:48 +0900

    0419

---
+++ item-service/src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,26 @@
+package hello.itemservice.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private Integer price;
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(Long id) {
+        this.id = id;
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
