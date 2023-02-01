a91353ffc2340c213d4883e8d63d74a5075eae7e
unknown <use770@naver.com>
2023-02-01 19:45:48 +0900

    20230201

--- web-application-server-master/src/main/java/webserver/RequestHandler.java
+++ web-application-server-master/src/main/java/webserver/RequestHandler.java
@@ -24,18 +24,7 @@ public class RequestHandler extends Thread {
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             BufferedReader br = new BufferedReader(new InputStreamReader(in));
-            // 요구사항 2. GET 방식으로 회원가입하기
-            String line = "http start";
-            while (!"".equals(line)) {
-                if (line.contains("/user/create?")) {
-                    String tokens[] = line.split("=|&");
-                    for (String s :
-                            tokens) {
-                        System.out.println("s = " + s);
-                    }
-                }
-            }
-            // 요구사항 1. index.html 응답하기
+
             String htmlFile = dispatcherFile(br);
 
 
@@ -84,14 +73,50 @@ public class RequestHandler extends Thread {
             }
             System.out.println(line);
 
+            // 요구사항 2. GET 방식으로 회원가입하기
+            if ( line.contains("GET") && line.contains("/user/create?") ) {
+                line = line.split("\\?")[1];
+                String tokens[] = line.split("&| ");
+                String userId = "";
+                String password = "";
+                String name = "";
+                String email = "";
+
+                for (String s : tokens) {
+                    if (s.contains("=")) {
+                        String[] mapToken = s.split("=");
+                        if (mapToken[0].equals("userId"))
+                            userId = mapToken[1];
+                        if (mapToken[0].equals("password"))
+                            password = mapToken[1];
+                        if (mapToken[0].equals("name"))
+                            name = mapToken[1];
+                        if (mapToken[0].equals("email"))
+                            email = mapToken[1];
+                    } else {
+                        continue;
+                    }
+                }
+
+                User user = new User(userId, password, name, email);
+                System.out.println("user = " + user);
+            }
+
+            // 요구사항 3. POST 방식으로 회원가입하기
+            if ( line.contains("POST") && line.contains("/user/create") ) {
+
+            }
+
             if (line.contains("Host:")) {
                 String[] tokens = line.split(" ");
                 host = tokens[1];
             }
 
+            // 요구사항 1. index.html 응답하기
             if (line.contains("HTTP/1.1")) {
                 String[] tokens = line.split(" ");
                 htmlFile = tokens[1];
+                System.out.println("tokens = " + tokens);
             }
 
         }
